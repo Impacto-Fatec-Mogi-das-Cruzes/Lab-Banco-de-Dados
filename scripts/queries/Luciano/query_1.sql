776364a0df5549ca5842d55cc348d045507116e8
LucianoAk <lucianotokuno@gmail.com>
2026-03-07 22:27:18 -0300

    feat: update queries

--- scripts/queries/Luciano/query_1.sql
+++ scripts/queries/Luciano/query_1.sql
@@ -1,7 +1,7 @@
--- Total de Produtos Comprados
+-- Total Comprado de cada produto
 SELECT
     p.PDT_NOME AS "Produto",
-    SUM(pp.PPD_QUANTIDADE) AS "Quantidade"
+    COALESCE(SUM(pp.PPD_QUANTIDADE), 0) AS "Quantidade Total"
 FROM PEDIDOS_PRODUTOS pp
 JOIN PRODUTOS p ON pp.PPD_PDT_ID = p.PDT_ID
 GROUP BY pp.PPD_PDT_ID, p.PDT_NOME
--- scripts/queries/Luciano/query_2.sql
+++ scripts/queries/Luciano/query_2.sql
@@ -1,6 +1,6 @@
--- Porcentagem de Pedidos Pago a Mais, Menos e Exatos
+-- Porcentagem de pedidos em que se foi pago a mais, a menos e exatamente o valor
 SELECT
-    COALESCE(ROUND(SUM(CASE WHEN p.PDD_VALOR_TOTAL - p.PDD_VALOR_PAGO > 0 THEN 1 END)*100 / COUNT(p.PDD_ID), 2), 0) AS "Pago a menos", 
-    COALESCE(ROUND(SUM(CASE WHEN p.PDD_VALOR_TOTAL - p.PDD_VALOR_PAGO = 0 THEN 1 END)*100 / COUNT(p.PDD_ID), 2), 0) AS "Exato",
-    COALESCE(ROUND(SUM(CASE WHEN p.PDD_VALOR_TOTAL - p.PDD_VALOR_PAGO < 0 THEN 1 END)*100 / COUNT(p.PDD_ID), 2), 0) AS "Pago a mais"
+    COALESCE(ROUND(SUM(CASE WHEN p.PDD_VALOR_TOTAL - p.PDD_VALOR_PAGO > 0 THEN 1 END)*100 / COUNT(p.PDD_ID), 2), 0) || '%' AS "A Menos", 
+    COALESCE(ROUND(SUM(CASE WHEN p.PDD_VALOR_TOTAL - p.PDD_VALOR_PAGO = 0 THEN 1 END)*100 / COUNT(p.PDD_ID), 2), 0) || '%' AS "Exato",
+    COALESCE(ROUND(SUM(CASE WHEN p.PDD_VALOR_TOTAL - p.PDD_VALOR_PAGO < 0 THEN 1 END)*100 / COUNT(p.PDD_ID), 2), 0) || '%' AS "A Mais"
 FROM PEDIDOS p;
