8aa72d9c091ee1be9c78d6adf430b30af35775d5
LucianoAk <lucianotokuno@gmail.com>
2026-04-10 15:40:45 -0300

    feat:luana query

--- scripts/queries/delivery2/Luana/query_1.sql
+++ scripts/queries/delivery2/Luana/query_1.sql
@@ -1,1 +1,12 @@
 -- Média da quantidade de Alfaces Lisas compradas por mês do ano de 2024
+
+select
+sum(hpp.HPPD_QUANTIDADE) as total,
+extract(month from hp.HPDD_DATA) as mes
+from HPEDIDOS_PRODUTOS hpp
+join PRODUTOS p on hpp.HPPD_PDT_ID = p.PDT_ID
+join HPEDIDOS hp on hpp.HPPD_PDD_ID = hp.HPDD_ID
+where p.PDT_NOME = 'Alface Lisa'
+and extract(year from hp.HPDD_DATA) = 2024
+group by extract(month from hp.HPDD_DATA)
+order by mes
--- scripts/queries/delivery2/Luana/query_2.sql
+++ scripts/queries/delivery2/Luana/query_2.sql
@@ -1,1 +1,10 @@
 -- Quantidade total de Alfaces Lisas, Crespas e Americanas compradas no ano de 2025
+
+select
+sum(case when p.PDT_NOME = 'Alface Lisa' then hpp.HPPD_QUANTIDADE end) as "Alface Lisa",
+sum(case when p.PDT_NOME = 'Alface Americana' then hpp.HPPD_QUANTIDADE end) as "Alface Americana",
+sum(case when p.PDT_NOME = 'Alface Crespa' then hpp.HPPD_QUANTIDADE end) as "Alface Crespa"
+from HPEDIDOS_PRODUTOS hpp
+join PRODUTOS p on hpp.HPPD_PDT_ID = p.PDT_ID
+join HPEDIDOS hp on hpp.HPPD_PDD_ID = hp.HPDD_ID
+where extract(year from hp.HPDD_DATA) = 2025
