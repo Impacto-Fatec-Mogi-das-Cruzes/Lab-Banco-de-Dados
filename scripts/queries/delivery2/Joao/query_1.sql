025ce4832f219af841b7fc2c165045cc761d15f6
LucianoAk <lucianotokuno@gmail.com>
2026-04-17 13:59:19 -0300

    feat: João queries for delievery 2

--- scripts/queries/delivery2/Joao/query_1.sql
+++ scripts/queries/delivery2/Joao/query_1.sql
@@ -1,12 +1,27 @@
--- Produtos Menos Vendidos
+-- Comparação entre Pedidos Pagos A mais, A menos e Exatamente de Cada Mês
+
+with all_pedidos as (
+   select p.pdd_data,
+          p.pdd_valor_total,
+          p.PDD_VALOR_PAGO
+     from pedidos_produtos ppd
+     join pedidos p
+   on ppd.ppd_pdd_id = p.pdd_id
+   union all
+   select hp.hpdd_data as pdd_data,
+          hp.HPDD_VALOR_TOTAL as pdd_valor_total,
+          hp.HPDD_VALOR_PAGO as pdd_valor_pago
+     from hpedidos_produtos hppd
+     join hpedidos hp
+   on hppd.hppd_pdd_id = hp.hpdd_id
+)
 
 SELECT 
-SUM (hpp.HPPD_QUANTIDADE) AS total,
-p.PDT_NOME AS produto
-FROM HPEDIDOS hp
-JOIN HPEDIDOS_PRODUTOS hpp ON hp.HPDD_ID = hpp.HPPD_PDD_ID
-JOIN PRODUTOS p ON hpp.HPPD_PDT_ID = p.PDT_ID
-where extract(month from hp.HPDD_DATA) = 12
-group by p.PDT_NOME
-order by total asc
-
+    TO_CHAR(TO_DATE(EXTRACT(MONTH FROM ap.PDD_DATA), 'MM'), 'MONTH') AS MES,
+    EXTRACT(MONTH FROM ap.pdd_data) as "Mês",
+    COALESCE(SUM(CASE WHEN ap.pdd_valor_total - ap.pdd_valor_pago > 0 THEN 1 END), 0) AS "A Menos", 
+    COALESCE(SUM(CASE WHEN ap.pdd_valor_total - ap.pdd_valor_pago = 0 THEN 1 END), 0) AS "Exato",
+    COALESCE(SUM(CASE WHEN ap.pdd_valor_total - ap.pdd_valor_pago < 0 THEN 1 END), 0) AS "A Mais"
+FROM all_pedidos ap
+GROUP BY EXTRACT(MONTH FROM ap.pdd_data)
+ORDER BY EXTRACT(MONTH FROM ap.pdd_data)
--- scripts/queries/delivery2/Joao/query_2.sql
+++ scripts/queries/delivery2/Joao/query_2.sql
@@ -1,11 +1,24 @@
--- Forncedores com Mais Pedidos nos Meses de Outubro
+-- Número de Pedidos Vendidos por Fornecedores por Ano
+
+WITH all_pedidos as (
+    SELECT
+        p.PDD_DATA,
+        p.PDD_ID,
+        p.PDD_FOC_ID
+    FROM PEDIDOS p
+    UNION ALL
+    SELECT 
+        hp.HPDD_DATA as PDD_DATA,
+        hp.HPDD_ID as PDD_ID,
+        hp.HPDD_FOC_ID as PDD_FOC_ID
+    FROM HPEDIDOS hp
+)
 
 SELECT
-    f.FOC_NOME AS "Nome do Fornecedor",
-    COUNT(hp.HPDD_ID) AS "Quantidade"
-FROM HPEDIDOS hp
-JOIN FORNECEDORES f 
-    ON hp.HPDD_FOC_ID = f.FOC_ID
-WHERE EXTRACT(MONTH FROM hp.HPDD_DATA) = 10
-GROUP BY f.FOC_NOME
-ORDER BY "Quantidade" DESC;
+    f.FOC_NOME as "Nome do Fornecedor",
+    COALESCE(COUNT(CASE WHEN EXTRACT(YEAR FROM ap.PDD_DATA) = 2024 THEN 1 END), 0) AS "Número de Pedidos de 2024",
+    COALESCE(COUNT(CASE WHEN EXTRACT(YEAR FROM ap.PDD_DATA) = 2025 THEN 1 END), 0) AS "Número de Pedidos de 2025",
+    COALESCE(COUNT(CASE WHEN EXTRACT(YEAR FROM ap.PDD_DATA) = 2026 THEN 1 END), 0) AS "Número de Pedidos de 2026"
+FROM all_pedidos ap
+JOIN FORNECEDORES f ON ap.PDD_FOC_ID = f.FOC_ID
+GROUP BY f.FOC_ID, f.FOC_NOME
