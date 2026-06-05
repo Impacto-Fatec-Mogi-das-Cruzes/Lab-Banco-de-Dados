826ac88ca7ac60a1e874b4db93bbfa0fef064841
LucianoAk <lucianotokuno@gmail.com>
2026-06-04 22:59:02 -0300

    fix: values as numbers

--- scripts/views/Luciano/createMView.sql
+++ scripts/views/Luciano/createMView.sql
@@ -6,10 +6,11 @@ AS
 SELECT 
     EXTRACT(month FROM ap.PDD_DATA) || '/' || EXTRACT(year FROM ap.PDD_DATA) AS "Mês e ano",
     COUNT(ap.PDD_ID) as "Total de pedidos",
-    TO_CHAR(SUM(ap.PDD_VALOR_TOTAL), 'FML999G999D00','NLS_NUMERIC_CHARACTERS = '',.''NLS_CURRENCY = ''R$''') AS "Valor total a ser pago no mês",
-    TO_CHAR(SUM(ap.PDD_VALOR_PAGO), 'FML999G999D00','NLS_NUMERIC_CHARACTERS = '',.''NLS_CURRENCY = ''R$''') AS "Valor total pago no mês",
-    TO_CHAR(SUM(ap.PDD_VALOR_TOTAL) / EXTRACT(DAY FROM LAST_DAY(ap.PDD_DATA)), 'FML999G999D00','NLS_NUMERIC_CHARACTERS = '',.''NLS_CURRENCY = ''R$''') AS " Preço médio diário dos pedidos",
-    TO_CHAR(SUM(ap.PDD_VALOR_PAGO) / EXTRACT(DAY FROM LAST_DAY(ap.PDD_DATA)), 'FML999G999D00','NLS_NUMERIC_CHARACTERS = '',.''NLS_CURRENCY = ''R$''') AS " Gasto médio diário dos pedidos",
+    SUM(ap.PDD_VALOR_TOTAL) AS "Valor total a ser pago no mês",
+    SUM(ap.PDD_VALOR_PAGO) AS "Valor total pago no mês",
+    SUM(ap.PDD_VALOR_PAGO) - SUM(ap.PDD_VALOR_TOTAL) AS "Diferença entre o pago e a ser pago no mês",
+    ROUND(SUM(ap.PDD_VALOR_TOTAL) / EXTRACT(DAY FROM LAST_DAY(ap.PDD_DATA)), 2) AS " Preço médio diário dos pedidos",
+    ROUND(SUM(ap.PDD_VALOR_PAGO) / EXTRACT(DAY FROM LAST_DAY(ap.PDD_DATA)), 2) AS " Gasto médio diário dos pedidos",
     CASE
         WHEN SUM(ap.PDD_VALOR_TOTAL) > AVG(SUM(ap.PDD_VALOR_TOTAL)) OVER () THEN 'maior'
         WHEN SUM(ap.PDD_VALOR_TOTAL) < AVG(SUM(ap.PDD_VALOR_TOTAL)) OVER () THEN 'menor'
@@ -22,7 +23,7 @@ SELECT
     END AS "Valor pago Acima, abaixo ou igual a média"
 FROM DW_USER.ALL_PEDIDOS_VIEW ap
 GROUP BY EXTRACT(month FROM ap.PDD_DATA), EXTRACT(year FROM ap.PDD_DATA), EXTRACT(DAY FROM LAST_DAY(ap.PDD_DATA))
-ORDER BY EXTRACT(year FROM ap.PDD_DATA), EXTRACT(month FROM ap.PDD_DATA)
+ORDER BY EXTRACT(year FROM ap.PDD_DATA), EXTRACT(month FROM ap.PDD_DATA);
 
 -- refresh na view
 BEGIN
@@ -33,3 +34,5 @@ END;
 
 SELECT *
 FROM DW_USER.PEDIDOS_MVIEW;
+
+DROP MATERIALIZED VIEW  DW_USER.PEDIDOS_MVIEW;
